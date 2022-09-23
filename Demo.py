65b4fcb95137fa40c428e9e5b3cd903f63d98749
Sandeepvsh369 <ccsandeep5@gmail.com>
2022-09-23 10:18:02 +0530

    firstcommit

---
+++ Demo.py
@@ -0,0 +1,32 @@
+"""
+def reverse_while_loop(s):
+    s1 = ''
+    length = len(s) - 1
+    while length >= 0:
+        s1 = s1 + s[length]
+        length = length - 1
+    return s1
+
+s2=reverse_while_loop('Sandeep');
+print (s2);
+"""
+
+class Solution(object):
+   def fizzBuzz(self, n):
+      """
+      :type n: int
+      :rtype: List[str]
+      """
+      result = []
+      for i in range(1,n+1):
+         if i% 3== 0 and i%5==0:
+            result.append("FizzBuzz")
+         elif i %3==0:
+            result.append("Fizz")
+         elif i% 5 == 0:
+            result.append("Buzz")
+         else:
+            result.append(str(i))
+      return result
+ob1 = Solution()
+print(ob1.fizzBuzz(30))
---
+++ add.py
@@ -0,0 +1,87 @@
+"""
+x = str(3)
+y = int(6)
+z = float(9)
+
+X = 'Tesla'
+
+print(X)
+print(x)
+print(y) 
+print(z)
+
+
+myvar = 'Tesla'
+Myvar = 'Nikola'
+print(Myvar, myvar)
+
+myOnlyVariable = 'Albert Einstein'
+
+a, b, c = "Leonardo", "Da", "Vinci"
+print(a, b, c)
+print(a, c, b)
+
+
+techs = ["Jobs", "Gates", "Jeff"]
+x,y,z = techs
+print(x,y,z)
+
+s = "awesome"
+def myfunc():
+    global s
+    s = "awful" 
+myfunc()
+print("Python is",s)
+
+name = 'tesla'
+print(name)
+print(type(name))
+
+
+list_data = ['an', 'example', 'of', 'a', 'list']
+print(list_data)
+list_data[3] = 'another'
+print(list_data)
+
+tuple_data = ('this', 'is', 'an', 'example', 'of', 'tuple')
+print(tuple_data)
+
+
+List = [1, 2, 4, 4, 3, 3, 3, 6, 5]
+print("Original list ",List)
+List[3] = 77
+print("Example to show mutablity ", List)
+
+
+x = range(9)
+print(x)
+
+x = {"Name" : "Tesla", "Age" : "Infinity"};
+print(x);
+
+
+x = "        sandeep is a good boy    ";
+print(x.upper());
+print(x.strip());
+print(x.replace("a", "i"));
+
+x = 5;
+y = 6;
+z = x+y;
+print(z);
+"""
+
+
+mylist = ["Tesla", "ALbie", "Musk"];
+mylist.insert(3, "DaVinci");
+mylist.insert(4, "Jobs");
+mylist[2] = "Elon";
+print(mylist[4]);
+print(mylist[3]);
+print(mylist);
+
+mytuple = ["Tesla", "Elon", "Twain"];
+mytuple[0] = "Nikola";
+print(mytuple);
+
+
